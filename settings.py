d4874dddf166a782ec547e6c1fc041175b3f924a
KhoixPham <160270269+KhoixPham@users.noreply.github.com>
2024-02-16 10:20:33 -0800

    Hello github

---
+++ settings.py
@@ -0,0 +1,20 @@
+# This file was created by: Khoi Pham
+
+TITLE = "My Lovely Game"
+
+# 32 grid wide
+WIDTH = 1024
+#24 grid high
+HEIGHT = 768
+TILESIZE = 32
+
+FPS = 30
+
+PLAYER_SPEED = 600
+
+SKYBLUE = (111, 186, 234)
+BGCOLOR = SKYBLUE
+LIGHTGREY = (100,100,100)
+GREEN = (0,255,0)
+YELLOW = (255,255,0)
+RED = ( 255,0,0)
---
+++ sprites.py
@@ -0,0 +1,115 @@
+# This file was created by: Khoi Pham
+
+# import modules
+import pygame as pg
+from pygame.sprite import Sprite
+from settings import *
+
+# create a player class
+
+class Player(Sprite):
+    # create init method / function to assign properties
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.vx, self.vy = 0, 0
+        self.x = x * TILESIZE
+        self.y = y * TILESIZE
+
+    # def move(self, dx=0, dy = 0):
+    # self.x += dx
+        #self. y +=dy
+    
+    # MOVEMENT with WASD
+    def get_keys(self):
+        self.vx, self.vy = 0, 0
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT] or keys[pg.K_a]:
+            self.vx = -PLAYER_SPEED
+        if keys[pg.K_RIGHT] or keys[pg.K_d]:
+            self.vx = PLAYER_SPEED
+        if keys[pg.K_UP] or keys[pg.K_w]:
+            self.vy = -PLAYER_SPEED
+        if keys[pg.K_DOWN] or keys[pg.K_s]:
+            self.vy = PLAYER_SPEED
+        if self.vx != 0 and self.vy !=0:
+            self.vx *= 0.7071
+            self.vy *= 0.7071
+    # COLLISION       
+    def collide_with_walls(self, dir):
+        if dir == 'x':
+            hits = pg.sprite.spritecollide(self, self.game.walls, False)
+            if hits:
+                if self.vx > 0:
+                    self.x = hits[0].rect.left - self.rect.width
+                if self.vx < 0:
+                    self.x = hits[0].rect.right
+                self.vx = 0
+                self.rect.x = self.x
+        if dir == 'y':
+            hits = pg.sprite.spritecollide(self, self.game.walls, False)
+            if hits:
+                if self.vy > 0:
+                    self.y = hits[0].rect.top - self.rect.height
+                if self.vy < 0:
+                    self.y = hits[0].rect.bottom
+                self.vy = 0
+                self.rect.y = self.y
+
+    def collide_with_obj(self, group, kill, desc):
+        hits = pg.sprite.spritecollide(self, group, kill)
+        if hits and desc == "coin":
+            self.image.fill(YELLOW)
+
+ 
+
+    def update(self):
+        #self.rect.x = self.vx * TILESIZE
+        #self.rect.y = self.vy * TILESIZE
+        self.get_keys()
+        self.x += self.vx * self.game.dt
+        self.y += self.vy * self.game.dt
+        self.rect.x = self.x
+        #ADD X COLLISION
+        self.collide_with_walls('x')
+        self.rect.y = self.y
+        #ADD Y COLLISION
+        self.collide_with_walls ('y')
+        self.collide_with_obj(self.game.coins, True, "coin")
+        self.rect.width = self.rect.width
+        self.rect.height = self.rect.height
+
+# create a wall class
+
+class Wall(Sprite):
+    # create init method for Wall
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.walls
+        Sprite.__init__(self, self.groups)
+        self.game = game
+        self.hitpoints = 100
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = self.x * TILESIZE
+        self.rect.y = self.y * TILESIZE
+class Coin(Sprite):
+    # create init method for Wall
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites, game.coins
+        Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = pg.Surface((TILESIZE, TILESIZE))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.x = self.x * TILESIZE
+        self.rect.y = self.y * TILESIZE
+
